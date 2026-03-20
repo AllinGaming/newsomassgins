a6feee79b1256a46883fd705c4978d042ba99ab3
Uros Radulovic <uros.radulovic23@gmail.com>
2026-03-20 15:18:58 +0100

    Fix workflow

--- vite.config.cjs
+++ vite.config.cjs
@@ -4,6 +4,7 @@ const path = require('node:path');
 module.exports = defineConfig(async () => {
   const { default: react } = await import('@vitejs/plugin-react');
   return {
+    base: '/newsomassgins/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src')
